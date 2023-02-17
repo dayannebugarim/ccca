c53d4fdf3e0c4777104a60be816e836648d9b916
dayannebugarim <dayannebugarim1@gmail.com>
2023-02-17 11:49:40 -0300

    feat: projeto parte 2

--- dist/src/Order.js
+++ dist/src/Order.js
@@ -4,17 +4,27 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const Cpf_1 = __importDefault(require("./Cpf"));
+const DefaultFreightCalculator_1 = __importDefault(require("./DefaultFreightCalculator"));
 const OrderItem_1 = __importDefault(require("./OrderItem"));
 class Order {
-    constructor(cpf) {
+    constructor(cpf, date = new Date(), freightCalculator = new DefaultFreightCalculator_1.default()) {
+        this.date = date;
+        this.freightCalculator = freightCalculator;
         this.cpf = new Cpf_1.default(cpf);
         this.orderItems = [];
+        this.freight = 0;
     }
     addItem(item, quantity) {
+        this.freight += this.freightCalculator.calculate(item) * quantity;
         this.orderItems.push(new OrderItem_1.default(item.idItem, item.price, quantity));
     }
     addCoupon(coupon) {
+        if (coupon.isExpired(this.date))
+            return;
         this.coupon = coupon;
+    }
+    getFreight() {
+        return this.freight;
     }
     getTotal() {
         let total = 0;
@@ -22,7 +32,7 @@ class Order {
             total += orderItem.getTotal();
         }
         if (this.coupon) {
-            total -= (total * this.coupon.percentage) / 100;
+            total -= this.coupon.calculateDiscount(total, this.date);
         }
         return total;
     }
--- dist/test/Order.test.js
+++ dist/test/Order.test.js
@@ -4,6 +4,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const Coupon_1 = __importDefault(require("../src/Coupon"));
+const DefaultFreightCalculator_1 = __importDefault(require("../src/DefaultFreightCalculator"));
+const FixedFreightCalculator_1 = __importDefault(require("../src/FixedFreightCalculator"));
 const Item_1 = __importDefault(require("../src/Item"));
 const Order_1 = __importDefault(require("../src/Order"));
 test("Deve criar um pedido vazio com CPF válido", function () {
@@ -34,4 +36,32 @@ test("Deve criar um pedido com 3 itens com um cupom de desconto", function () {
     order.addCoupon(new Coupon_1.default("VALE20", 20));
     const total = order.getTotal();
     expect(total).toBe(128);
+});
+test("Deve criar um pedido com 3 itens com um cupom de desconto expirado", function () {
+    const cpf = "839.435.452-10";
+    const order = new Order_1.default(cpf, new Date("2021-12-10"));
+    order.addItem(new Item_1.default(1, "Música", "CD", 30), 3);
+    order.addItem(new Item_1.default(2, "Vídeo", "DVD", 50), 1);
+    order.addItem(new Item_1.default(3, "Vídio", "VHS", 10), 2);
+    order.addCoupon(new Coupon_1.default("VALE20", 20, new Date("2021-12-01")));
+    const total = order.getTotal();
+    expect(total).toBe(160);
+});
+test("Deve criar um pedido com 3 itens com o cálculo do frete com a estratégia default", function () {
+    const cpf = "839.435.452-10";
+    const order = new Order_1.default(cpf, new Date(), new DefaultFreightCalculator_1.default());
+    order.addItem(new Item_1.default(4, "Instrumentos Musicais", "Guitarra", 1000, 100, 30, 10, 3), 1);
+    order.addItem(new Item_1.default(5, "Instrumentos Musicais", "Amplificador", 5000, 100, 50, 50, 20), 1);
+    order.addItem(new Item_1.default(6, "Acessórios", "Cabo", 30, 10, 10, 10, 0.9), 3);
+    const freight = order.getFreight();
+    expect(freight).toBe(260);
+});
+test("Deve criar um pedido com 3 itens com o cálculo do frete com a estratégia fixo", function () {
+    const cpf = "839.435.452-10";
+    const order = new Order_1.default(cpf, new Date(), new FixedFreightCalculator_1.default());
+    order.addItem(new Item_1.default(4, "Instrumentos Musicais", "Guitarra", 1000, 100, 30, 10, 3), 1);
+    order.addItem(new Item_1.default(5, "Instrumentos Musicais", "Amplificador", 5000, 100, 50, 50, 20), 1);
+    order.addItem(new Item_1.default(6, "Acessórios", "Cabo", 30, 10, 10, 10, 0.9), 3);
+    const freight = order.getFreight();
+    expect(freight).toBe(50);
 });
